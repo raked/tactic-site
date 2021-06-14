59991f27e954db46e95c561c6b8848fd27c7da66
raked <raked@users.noreply.github.com>
2021-06-14 18:03:19 -0400

    Update Nav.js

--- tactic-site/src/Nav.js
+++ tactic-site/src/Nav.js
@@ -1,12 +1,12 @@
 function Nav() {
     return (
-        <div className="d-flex-inline justify-content-around">
-            <nav className="navbar navbar-light navbar-expand-lg" style={{ backgroundColor: "#cfe8fb" }}>
+        <div className="d-flex-inline justify-content-around" style={{ backgroundColor: "#cfe8fb" }}>
+            <nav className="navbar navbar-light navbar-expand-lg">
                 <div className="container-fluid">
                     
-                        <a href="#music" className="link-dark ms-5">Music</a>
+                        <a href="#music" className="nav-link link-dark ms-5">Music</a>
                         <div className="lead">Tactic</div>
-                        <a href="#contact" className="link-dark me-5">Contact</a>
+                        <a href="#contact" className="nav-link link-dark me-5">Contact</a>
                     
                 </div>
             </nav>
