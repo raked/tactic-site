1a8938b87c00c48215981a20682c4ff533e287c7
raked <raked@users.noreply.github.com>
2021-06-14 00:25:20 -0400

    Did some more work on the nav
    but decided I should just build the bootstrap static site first and then convert to react later

--- tactic-site/src/Nav.js
+++ tactic-site/src/Nav.js
@@ -1,7 +1,15 @@
 function Nav() {
     return (
         <div>
-            
+            <nav className="navbar navbar-light navbar-expand-lg" style={{ backgroundColor: "#cfe8fb" }}>
+                <div className="container-fluid">
+                    <ul>
+                        <li><a href="#music">Music</a></li>
+                        <li><a href="#">Tactic</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </div>
+            </nav>
         </div>
     );
 }
