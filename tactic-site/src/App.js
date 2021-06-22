a69904f2fe2f9926eeadd1de56588390af2929ef
raked <raked@users.noreply.github.com>
2021-06-22 19:36:40 -0400

    Example card where i want it

--- tactic-site/src/App.js
+++ tactic-site/src/App.js
@@ -1,4 +1,4 @@
-import Nav from'./Nav'
+import Nav from './Nav'
 import Body from './Body'
 import './App.css';
 
@@ -6,16 +6,16 @@ function App() {
   return (
     <div className='App'>
       <head>
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link href="https://fonts.googleapis.com/css2?family=Mate+SC&display=swap" rel="stylesheet" /> 
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Mate+SC&display=swap" rel="stylesheet" />
       </head>
       <header>
         <Nav />
       </header>
       <body>
         <Body />
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
       </body>
     </div>
   );
--- tactic-site/src/Body.js
+++ tactic-site/src/Body.js
@@ -2,10 +2,17 @@ function Body() {
     return (
         <div>
             <section id="music">
-
+                <div className="d-flex text-center justify-content-center align-items-center min-vh-100 bg-dark">
+                    <div class="card" style={{ width: "18rem", height: "20rem" }}>
+                        <img src="..." class="card-img-top" alt="..." />
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
             </section>
             <section id="contact">
-                
+
             </section>
         </div>
     );
