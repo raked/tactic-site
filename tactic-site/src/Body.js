c89bce2126c8a5d3841259e69e95c798df71fd84
raked <raked@users.noreply.github.com>
2021-06-22 20:28:21 -0400

    Card section done

--- tactic-site/src/Body.js
+++ tactic-site/src/Body.js
@@ -1,13 +1,38 @@
 function Body() {
     return (
         <div>
-            <section id="music">
-                <div className="d-flex text-center justify-content-center align-items-center min-vh-100 bg-dark">
-                    <div class="card" style={{ width: "18rem", height: "20rem" }}>
-                        <img src="..." class="card-img-top" alt="..." />
-                        <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        </div>
+            <section id="music" className="bg-dark">
+                <h1 className="text-center text-white pt-5" style={{ fontFamily: "Mate SC" }}>Check out my music below</h1>
+                <div className="d-flex text-center justify-content-center align-items-center min-vh-100">
+                    <div className="p-3">
+                        <a href="https://soundcloud.com/andyv15/rich-the-kid-splashin-prod-tactic" className="link-primary" target="_blank" rel="noreferrer">
+                            <div className="card" style={{ width: "18rem" }}>
+                                <img src={"/assets/images/song_0.PNG"} className="card-img-top" alt="Rich the Kid - Splashin (prod. Tactic)" />
+                                < div className="card-body" >
+                                    <p className="card-text">Rich the Kid - Splashin (prod. Tactic)</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="p-3">
+                        <a href="https://soundcloud.com/andyv15/atmos" className="link-primary" target="_blank" rel="noreferrer">
+                            <div className="card" style={{ width: "18rem" }}>
+                                <img src={"/assets/images/song_1.PNG"} className="card-img-top" alt="Atmos" />
+                                < div className="card-body" >
+                                    <p className="card-text">Atmos</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="p-3">
+                        <a href="https://soundcloud.com/andyv15/betrayal" className="link-primary" target="_blank" rel="noreferrer">
+                            <div className="card" style={{ width: "18rem" }}>
+                                <img src={"/assets/images/song_2.PNG"} className="card-img-top" alt="Betrayal" />
+                                < div className="card-body" >
+                                    <p className="card-text">Betrayal</p>
+                                </div>
+                            </div>
+                        </a>
                     </div>
                 </div>
             </section>
