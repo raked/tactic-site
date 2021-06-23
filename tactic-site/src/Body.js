ec584a6eed191eca19a55efde43d7c4d40e4780c
raked <raked@users.noreply.github.com>
2021-06-22 20:44:19 -0400

    Finished up contact section

--- tactic-site/src/Body.js
+++ tactic-site/src/Body.js
@@ -3,7 +3,7 @@ function Body() {
         <div>
             <section id="music" className="bg-dark">
                 <h1 className="text-center text-white pt-5" style={{ fontFamily: "Mate SC" }}>Check out my music below</h1>
-                <div className="d-flex text-center justify-content-center align-items-center min-vh-100">
+                <div className="d-flex flex-column flex-lg-row text-center justify-content-center align-items-center min-vh-100">
                     <div className="p-3">
                         <a href="https://soundcloud.com/andyv15/rich-the-kid-splashin-prod-tactic" className="link-primary" target="_blank" rel="noreferrer">
                             <div className="card" style={{ width: "18rem" }}>
@@ -36,10 +36,10 @@ function Body() {
                     </div>
                 </div>
             </section>
-            <section id="contact">
+            <section id="contact" className="bg-dark">
                 <div className="container text-center p-3">
-                    <a href="https://www.github.com/raked" target="_blank" rel="noreferrer">
-                        <img src="assets/images/logo.png" alt="Soundcloud logo" />
+                    <a href="https://soundcloud.com/andyv15" target="_blank" rel="noreferrer">
+                        <img src="assets/images/logo.png" alt="Soundcloud logo" style={{ maxHeight: "100px" }} />
                     </a>
                 </div>
             </section>
